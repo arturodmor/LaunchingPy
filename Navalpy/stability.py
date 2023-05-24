bb36f9bb478dbe27619bd50e738422a54e7c274a
Arturo <rodriguezmoranarturo@gmail.com>
2023-05-24 18:45:29 +0200

    [FIX] stability class

--- Navalpy/stability.py
+++ Navalpy/stability.py
@@ -1,23 +1,20 @@
 class Stability():
-    def __init__(self, Height, Volume_ship, T_inertia, L_inertia, GMT, BMT, KMT, KB, KG):
+    def __init__(self, Height, Volume_ship, T_inertia, L_inertia, KB, KG):
         self.Height = Height
         self.volume_ship = Volume_ship
         self.T_inertia = T_inertia
         self.L_inertia = L_inertia
-        self.gmt = GMT
-        self.BMT = BMT
-        self.KMT = KMT
         self.KB = KB
         self.KG = KG
 
     def get_BMT(self):
-        self.BMT = self.T_inertia/self.volume_ship
-        return self.BMT
+        BMT = self.T_inertia/self.volume_ship
+        return BMT
     
     def get_KMT(self):
-        self.KMT = self.BMT + self.KB
-        return self.KMT
+        KMT = self.BMT + self.KB
+        return KMT
     
     def get_GMT(self):
-        self.GMT =self.KMT + self.KG
-        return self.GMT
+        GMT =self.KMT + self.KG
+        return GMT
--- ship.py
+++ ship.py
@@ -1,1 +1,12 @@
-from Navalpy import stability
+from Navalpy.stability import Stability
+
+ship = Stability(Height = 8,
+                 Volume_ship =24, 
+                 T_inertia = 1344234, 
+                 L_inertia = None, 
+                 KB = 7, 
+                 KG = 3)
+
+barco = ship.get_BMT()
+
+print(barco)
