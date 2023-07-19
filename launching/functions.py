6eb664238fd14f4909e654f4ab008b8c5c6728a8
Arturo Rodriguez <rodriguezmoranarturo@gmail.com>
2023-07-19 23:01:49 +0200

    [ADD] max_ton_key functinction

---
+++ launching/functions.py
@@ -0,0 +1,8 @@
+import math
+
+def max_ton_key(p, secure_coef, alpha_rad, N_eels):
+
+    alpha_rad = math.radians(5)
+    F_key = p*secure_coef*(math.sin(alpha_rad)-(f0*math.cos(alpha_rad)))/(N_eels)
+
+    return F_key
