f400dc42a5664e0f5e5b4dba867e29f2d4291768
Arturo Rodriguez <rodriguezmoranarturo@gmail.com>
2023-07-19 23:08:12 +0200

    [UPDATE] max_ton_key. add f0 parameter

--- launching/functions.py
+++ launching/functions.py
@@ -1,6 +1,6 @@
 import math
 
-def max_ton_key(p, secure_coef, alpha_rad, N_eels):
+def max_ton_key(p, secure_coef, alpha_rad, f0, N_eels):
 
     alpha_rad = math.radians(5)
     F_key = p*secure_coef*(math.sin(alpha_rad)-(f0*math.cos(alpha_rad)))/(N_eels)
