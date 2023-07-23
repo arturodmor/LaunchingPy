d13809981521fd6d74ff9736fbb6851321ca07a7
Arturo Rodriguez <rodriguezmoranarturo@gmail.com>
2023-07-23 19:44:09 +0200

    [UPDATE] library changes. Add tonnels

--- launching/functions.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def max_ton_key(p, secure_coef, alpha_rad, f0, N_eels):
-
-    alpha_rad = math.radians(5)
-    F_key = p*secure_coef*(math.sin(alpha_rad)-(f0*math.cos(alpha_rad)))/(N_eels)
-
-    return F_key
