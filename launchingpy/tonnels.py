bf44df501bfd9dbffa7d7843e13046c9bbcc61bd
Arturo <rodriguezmoranarturo@gmail.com>
2023-07-30 20:07:35 +0200

    [update] replace wrong input max_ton_key function

--- launchingpy/tonnels.py
+++ launchingpy/tonnels.py
@@ -1,8 +1,8 @@
 import math
 
-def max_ton_key(p, secure_coef, alpha_rad, f0, N_eels):
+def max_ton_key(p, secure_coef, alpha, f0, N_eels):
 
-    alpha_rad = math.radians(5)
+    alpha_rad = math.radians(alpha)
     F_key = p*secure_coef*(math.sin(alpha_rad)-(f0*math.cos(alpha_rad)))/(N_eels)
 
     return F_key
