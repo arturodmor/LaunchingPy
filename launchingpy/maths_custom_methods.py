da5e95a1a92caeb7d71a5b08a9c97a049b49a1e5
Arturo <rodriguezmoranarturo@gmail.com>
2023-07-30 19:58:30 +0200

    [update] now we get librariy imports in notebooks

--- launchingpy/maths_custom_methods.py
+++ launchingpy/maths_custom_methods.py
@@ -2,8 +2,4 @@ import math
 
 def pythagoras_method(a,hyp):
     if isinstance(a,list):
-        """for instance in a:
-            b = math.sqrt(hip**2-a**2)
-            b.append(b)
-            return b"""
         return {'b': [math.sqrt(hyp**2-x**2) for x in a]}
