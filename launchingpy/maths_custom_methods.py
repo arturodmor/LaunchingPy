d1428d2bc4b47e2c514440992e1903189185dd2d
Arturo <rodriguezmoranarturo@gmail.com>
2023-08-10 22:15:01 +0200

    [update] maths pythagoras method

--- launchingpy/maths_custom_methods.py
+++ launchingpy/maths_custom_methods.py
@@ -2,4 +2,4 @@ import math
 
 def pythagoras_method(a,hyp):
     if isinstance(a,list):
-        return {'b': [math.sqrt(hyp**2-x**2) for x in a]}
+        return [math.sqrt(hyp**2-x**2) for x in a]
